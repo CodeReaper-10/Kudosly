65972b4255a58ce3a1a84e3ae175a9377c3418be
SubhadipDas08 <subhadipdas724@gmail.com>
2023-02-19 16:54:50 +0530

    UI enhancements and navbar fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactUsComponent } from './components/contact-us/contact-us.component';
-import { FeaturesComponent } from './components/features/features.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { PointsTrackerComponent } from './components/points-tracker/points-tracker.component';
 import { RewardsCatalogComponent } from './components/rewards-catalog/rewards-catalog.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { FeaturesComponent } from './components/features/features.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
@@ -14,9 +14,8 @@ const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'home', component: RewardsCatalogComponent },
   { path: 'points-tracker', component: PointsTrackerComponent },
-  { path: 'features', component: FeaturesComponent },
   { path: 'contact-us', component: ContactUsComponent },
-
+  { path: 'features', component: FeaturesComponent },
 ];
 
 @NgModule({
