2b1dca99346c8734c33d09d4fc9b6ab28c068a21
CodeReaper-10 <adipta1010@gmail.com>
2023-02-21 02:06:38 +0530

    Fixed routing issues and repositioned navbar in points-tracker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { SignupComponent } from './components/signup/signup.component';
 import { RewardsCatalogComponent } from './components/rewards-catalog/rewards-catalog.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { PointsTrackerComponent } from './components/points-tracker/points-tracker.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
@@ -19,13 +21,14 @@ import { PointsTrackerComponent } from './components/points-tracker/points-track
     RewardsCatalogComponent,
     LandingPageComponent,
     PointsTrackerComponent,
+    FeaturesComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
